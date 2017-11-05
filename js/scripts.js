a9199e85d9382b8f17a418fd388e0df43a30a570
Kepha Okari <kephaokari@gmail.com>
2017-11-05 18:01:46 +0300

    added button that refreshes page for the new game to begin using jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
 // this will hide the input fields for the players names
-$(document).ready(function(){
-    $("button#playButton").click(function(e){
+/*$(document).ready(function(){
+*/    $("button#playButton").click(function(e){
     	e.preventDefault();
         $("div#player-names-section").toggle();
     });
-});
+
+    // refresh the page for new players or new game
+    $('.newGame').click(function() {
+    location.reload();
+	});
+
+/*});*/// end ready
+
+
+// Business Logic
+function randomGen(){
+	var diceNumber = Math.floor((Math.random()*6)+1);
+	return diceNumber;
+}
