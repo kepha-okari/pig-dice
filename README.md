5348785b8895818e7e414c5df1da125bb3941697
Kepha Okari <kephaokari@gmail.com>
2017-11-06 16:48:07 +0300

    added script to enter and store players details

--- README.md
+++ README.md
@@ -1,34 +1,34 @@
-Kipng'etich Ng'eno [5:54 PM] 
-# AUTHOR
 
-#### Kepha Okari
+# By Kepha Okari
+
+#### 
 
 # PROJECT NAME
 
-#### Pig Dice
+### Pig Dice
 
-# DESCRIPTION
+## DESCRIPTION
 
 #### This is a computer based a dice game governed by the following rules
 
 ##### 
 * When a player rolls a 1, their total score reverts to 0 and it becomes the next player's turn.
 * When the player rolls a 2 - 6, the score is added to their turn total and they can continue to play
-* When the player chooses to "hold", their turn total is added to their total score, and it becomes the next player's turn.
+* When the player chooses to "hold", their turn total is added to their total score, and the other player takes the turn.
 * The player who first scores 100 or more points wins.
 # Installation
 
 #### The game is easily available on Github
 
-# How to Play 
+## How to Play 
 
 #### A two player game, simply click on the start button to initiate.
 
-# CREDITS
+## CREDITS
 
 ####  StackOverflow.com and W3 schools.
 
-#Technoligies
+## Technoligies
 
 ##### 
 * HTML
@@ -37,7 +37,7 @@ Kipng'etich Ng'eno [5:54 PM]
 * Javascript.
 * JQuery
 
-# License
+## License
 
 *MIT License
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,74 @@
-// this will hide the input fields for the players names
-/*$(document).ready(function(){
-*/    $("button#playButton").click(function(e){
-    	e.preventDefault();
-        $("div#player-names-section").toggle();
-    });
 
-    // refresh the page for new players or new game
+
+
+// BUSINESS LOGIC
+function Player(playerName, turnScore, totalScore) {
+  this.playerName = playerName;
+  this.turnScore = turnScore;
+  this.totalScore = totalScore;
+}
+
+Player.prototype.roll = function() {
+  var diceValues = [1, 2, 3, 4, 5, 6];
+  var rollValue = diceValues[Math.floor(Math.random() * diceValues.length)];
+  if (rollValue === 1) {
+    this.turnScore = 0;
+  } else {
+    this.turnScore = this.turnScore + rollValue;
+  };
+  return rollValue;
+}
+
+Player.prototype.score = function() {
+  this.totalScore = this.turnScore + this.totalScore;
+  this.turnScore = 0;
+}
+
+
+
+// USER INTERFACE
+
+// this will hide the input fields for the players names
+$(document).ready(function(){
+
+	// refresh the page for new players or new game
     $('.newGame').click(function() {
     location.reload();
+    $("#player1Name").val("");
+    $("#player2Name").val("");
+
 	});
 
-/*});*/// end ready
+  	$("button#playButton").click(function(e){
+    	e.preventDefault();
+
+        $("div#player-names-section").toggle();
+	        var player1Name = $("#player1Name").val();
+			var player2Name = $("#player2Name").val();
+			var player1 = new Player (player1Name,0,0);
+			var player2 = new Player (player2Name,0,0);
 
 
-// Business Logic
-function randomGen(){
-	var diceNumber = Math.floor((Math.random()*6)+1);
-	return diceNumber;
-}
+	  $(".player1NameOutput").text(player1.playerName);
+	  $(".player2NameOutput").text(player2.playerName);
+	  $(".player1-total-score").html("<span class='player1-total-score'>" + player1.totalScore + "</span>");
+    });
+
+    $("#roll1").click(function(event) {
+    event.preventDefault();
+    var player1RolledNumber = player1.roll();
+    if (player1RolledNumber === 1) {
+     $(".player1").fadeOut(200);
+      //$("#well-play1").fadeOut(900);
+      $("player2").show(900);
+    }
+    $(".player1-rolled-number").text(player1RolledNumber);
+    $(".player1-turn-score").text(player1.turnScore);
+  });
+
+
+
+ 
+
+});
+// end ready
