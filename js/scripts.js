0cb9155f814e9bf0d96ce03822ce7967e81e24f2
Kepha Okari <kephaokari@gmail.com>
2017-11-08 22:45:42 +0300

    image rolls

--- js/scripts.js
+++ js/scripts.js
@@ -23,10 +23,16 @@ Player.prototype.score = function() {
   this.totalScore += this.turnScore ;
   this.turnScore = 0;
 }
+
 Player.prototype.totalScore = function() {
   this.totalScore += this.turnScore ;
   return this.totalScore;
 }
+
+  // to change image of rolled dice
+  function showDice (dice) {
+    $("img").attr("src","img/" + dice + ".png").hide().fadeIn();
+  }
 
 // USER INTERFACE
 // this will hide the input fields for the players names
@@ -46,15 +52,15 @@ $(document).ready(function(){
 
   	$("button#playButton").click(function(event){
     	event.preventDefault();
-
+        // stores the names of players
         $("div#player-names-section").toggle();
 	        var player1Name = $("#player1Name").val();
-			var player2Name = $("#player2Name").val();
+		    	var player2Name = $("#player2Name").val();
 
 			player1 = new Player (player1Name,0,0);
 			player2 = new Player (player2Name,0,0);
 
-		//stores and display the names of players and the initial score
+		//display the names of players and the initial score
 	  $(".player1NameOutput").text(player1.playerName);
 	  $(".player2NameOutput").text(player2.playerName);
 	  $(".player1-total-score").html("<span class='player1-total-score'>" + player1.totalScore + "</span>");
@@ -68,10 +74,17 @@ $(document).ready(function(){
     		$(".player1-turn-score").text(player1.turnScore);
     		$(".player1-total-score").text(player1.score());
 
+          //display rolled image 
+          showDice (player1RolledNumber);
+
     		alert("Oop! You rolled a 1, it is   " + player2.playerName + "'s turn");// display turn is over
     		$("#player1").show().fadeOut(1200);
     		$("#player2").show();
-    	} 		   
+    	} 	
+
+        //display rolled image 
+        showDice (player1RolledNumber);
+
 		   	$(".player1-rolled-number").text(player1RolledNumber);
 		    $(".player1-turn-score").text(player1.turnScore);
 		    $(".player1-total-score").text(player1.totalScore);
@@ -105,11 +118,17 @@ $(document).ready(function(){
     	if (player2RolledNumber === 1) {
     		$(".player2-turn-score").text(player2.turnScore);
     		$(".player2-total-score").text(player2.score());
+
+            //display rolled image 
+            showDice (player2RolledNumber);
     		/*alert(player2.score);*/
     		alert("Oop! You rolled a 1, it is   " + player1.playerName + "'s turn");// display turn is over// display turn is over
     		$("#player2").show().fadeOut(1200);
     		$("#player1").show();
-    	} 		   
+    	} 
+            //display rolled image 
+            showDice (player2RolledNumber);
+
 		   	$(".player2-rolled-number").text(player2RolledNumber);
 		    $(".player2-turn-score").text(player2.turnScore);
 		    $(".player2-total-score").text(player2.totalScore);
